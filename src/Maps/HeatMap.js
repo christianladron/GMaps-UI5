ad13082e7a14001628901214995def74b080c891
Christian Ladrón <christianlgr@gmail.com>
2014-08-12 00:38:31 -0500

    Fix L

--- src/Maps/HeatMap.js
+++ src/Maps/HeatMap.js
@@ -1,3 +1,18 @@
+    //Google Maps control for SAP UI5
+    //Copyright (C) 2014  Christian Ladrón de Guevara Reyes
+
+    //This program is free software: you can redistribute it and/or modify
+    //it under the terms of the GNU General Public License as published by
+    //the Free Software Foundation, either version 3 of the License, or
+    //(at your option) any later version.
+
+    //This program is distributed in the hope that it will be useful,
+    //but WITHOUT ANY WARRANTY; without even the implied warranty of
+    //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    //GNU General Public License for more details.
+
+    //You should have received a copy of the GNU General Public License
+    //along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 	jQuery.sap.require('sap.ui.core.Element','mexbalia.Maps.Map','mexbalia.Maps.Location'); 
 	sap.ui.core.Element.extend("mexbalia.Maps.HeatMap",{
--- src/Maps/Map.js
+++ src/Maps/Map.js
@@ -1,3 +1,18 @@
+    //Google Maps control for SAP UI5
+    //Copyright (C) 2014  Christian Ladrón de Guevara Reyes
+
+    //This program is free software: you can redistribute it and/or modify
+    //it under the terms of the GNU General Public License as published by
+    //the Free Software Foundation, either version 3 of the License, or
+    //(at your option) any later version.
+
+    //This program is distributed in the hope that it will be useful,
+    //but WITHOUT ANY WARRANTY; without even the implied warranty of
+    //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    //GNU General Public License for more details.
+
+    //You should have received a copy of the GNU General Public License
+    //along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 	jQuery.sap.require('mexbalia.Maps.GMapsLoader','sap.ui.core.Control');
 
@@ -22,9 +37,9 @@
 			}
 		},
 		renderer:function(oRm,oControl){
-			oRm.write("<div id=\"mapa"+oControl.getId()+"\" style=\"height:"+ "100%" +";width:"+ "100%" +";\"> </div>");
-			jQuery(oControl.getUIArea().getRootNode()).width(oControl.getWidth());
-			jQuery(oControl.getUIArea().getRootNode()).height(oControl.getHeight());
+			oRm.write("<div ");
+			oRm.writeControlData(oControl);
+			oRm.write("id=\"mapa"+oControl.getId()+"\" style=\"height:"+ oControl.getHeight() +";width:"+ oControl.getWidth() +";\"> </div>");
 		},
 		onAfterRendering:function(){
 			if(typeof google === "undefined" || typeof google.maps === "undefined" ){
@@ -36,12 +51,14 @@
 		},
 		firstRendering:function(){
 			var Center = this.getCenter();
+			jQuery(this.getDomRef().parentElement).width(this.getWidth());
+			jQuery(this.getDomRef().parentElement).height(this.getHeight());
 			var mapOptions = {
 				zoom: this.getZoom(),
 				center: new google.maps.LatLng(Center.latitude, Center.longitude),
 				mapTypeId: google.maps.MapTypeId.ROADMAP
 			};
-			var map = new google.maps.Map(document.getElementById("mapa"+this.getId()), mapOptions);
+			var map = new google.maps.Map(this.getDomRef(), mapOptions);
 			this.setMap(map);
 			this.fireMapCreated();
 		},
